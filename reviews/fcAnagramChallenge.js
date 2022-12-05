b360157cb0291e98dc7ccb2b0385b2db385c5e5b
mollycarroll <mollyajcarroll@gmail.com>
2022-12-04 23:55:10 -0700

    add reviews directory for refresher exercises from this course; solve validAnagram challenge for frequency counter pattern

---
+++ reviews/fcAnagramChallenge.js
@@ -0,0 +1,29 @@
+/*
+Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed 
+by rearranging the letters of another, such as cinema, formed from iceman.
+*/
+
+const isAnagram = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    let charCount1 = {};
+    let charCount2 = {};
+
+    for (let c of arr1) {
+        charCount1[c] = (charCount1[c] || 0) + 1;
+    }
+
+    for (let d of arr2) {
+        charCount2[d] = (charCount2[d] || 0) + 1;
+    }
+
+    for (let key in charCount1) {
+        if ((!!key in charCount2) || charCount2[key] !== charCount1[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
