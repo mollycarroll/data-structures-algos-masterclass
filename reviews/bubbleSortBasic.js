02f6eb8d0265b93a0b8380c0a0f42dec46fab97b
mollycarroll <mollyajcarroll@gmail.com>
2022-12-12 06:14:46 -0700

    set up bubbleSort basic implementation file

---
+++ reviews/bubbleSortBasic.js
@@ -0,0 +1,15 @@
+/*
+Basic implementation of bubble sort
+
+https://www.geeksforgeeks.org/bubble-sort/
+Run a nested for loop to traverse the input array using two variables i and j, such that 0 ≤ i < n-1 and 0 ≤ j < n-i-1
+If arr[j] is greater than arr[j+1] then swap these adjacent elements, else move on
+Print the sorted array
+
+*/
+
+const bubbleSort = (arr) => {
+
+
+    return arr;
+}
