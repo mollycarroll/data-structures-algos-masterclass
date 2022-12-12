9335458c6c5e0d56417a875c1a68714b3f25a626
Molly Carroll <molly.carroll@kroger.com>
2022-12-12 06:53:33 -0700

    finish basic bubble sort function

--- reviews/bubbleSortBasic.js
+++ reviews/bubbleSortBasic.js
@@ -9,7 +9,15 @@ Print the sorted array
 */
 
 const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length-1; i++) {
 
+        for (let j = 0; j < arr.length-i-1; j++) {
+
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
 
     return arr;
 }
